a0eca39c18f474b14f68bbcb347c5478d59335d2
GerhardGDCO05 <gerhardcastro8a18@gmail.com>
2026-02-15 14:25:00 -0400

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/Shalom-Trendy/trendyfront/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/front-Shalom/'
+    : '/',
   transpileDependencies: true,
   productionSourceMap: false
 })
